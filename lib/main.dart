0b38533b3fe011bc0bb286b919b52b1520bac34a
MasterTVC <googlythattuyet@gmail.com>
2020-11-28 01:08:42 +0700

    update readme

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.blue,
-      //   visualDensity: VisualDensity.adaptivePlatformDensity,
-      // ),
-      // home: CounterView(),
       home: NoteView(),
     );
   }
